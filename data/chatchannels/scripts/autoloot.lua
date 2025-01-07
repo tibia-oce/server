de0a21598dc54442ce9c460dfd279ae71785eb4a
Jordan Hoare <jordanhoare0@gmail.com>
2025-01-07 17:58:15 +0800

    chore: remove out of scope changes

--- data/chatchannels/scripts/autoloot.lua
+++
@@ -1,8 +0,0 @@
-function onSpeak(player, type, message)
-    local playerAccountType = player:getAccountType()
-    if playerAccountType == ACCOUNT_TYPE_NORMAL then
-        player:sendCancelMessage("You can not speak here.")
-        return false
-    end
-    return type
-end
