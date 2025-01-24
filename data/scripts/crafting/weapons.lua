d28c5dc10cb7e38a958558858770af8bb7866653
Jordan Hoare <57948967+jordanhoare@users.noreply.github.com>
2025-01-24 20:52:30 +0800

    chore: refresh player material counts and improve crafting options (#34)
    * chore: simplify crafting categories
    
    * chore: construct an all category which contains all other table items
    
    * fix: update player material counts on successful craft
    
    * chore: simplify crafting categories

---
+++ data/scripts/crafting/weapons.lua
@@ -0,0 +1,153 @@
+Crafting.weapons = {
+    {
+        id = 7383,
+        name = "Relic Sword",
+        level = 15,
+        cost = 1000,
+        count = 1,
+        materials = {
+            {id = 24708, count = 4}, 
+            {id = 11234, count = 3}, 
+            {id = 26434, count = 1}, 
+            {id = 26433, count = 2},  
+        }
+    },
+    {
+        id = 15451,
+        name = "Warrior's Axe",
+        level = 15,
+        cost = 1000,
+        count = 1,
+        materials = {
+            {id = 15452, count = 2}, 
+            {id = 15426, count = 3}, 
+            {id = 26434, count = 1}, 
+            {id = 26433, count = 2},  
+        }
+    },
+    {
+        id = 23542,
+        name = "Metal Bat",
+        level = 15,
+        cost = 1000,
+        count = 1,
+        materials = {
+            {id = 23567, count = 2},  
+            {id = 5880, count = 2}, 
+            {id = 26434, count = 1}, 
+            {id = 26433, count = 2},  
+        }
+    },
+    {
+        id = 2522,
+        name = "Great Shield",
+        level = 20,
+        cost = 500000,
+        count = 1,
+        materials = {
+            {id = 5920, count = 300}, 
+            {id = 5882, count = 300}, 
+            {id = 10020, count = 3}, 
+            {id = 5919, count = 5}, 
+            {id = 28127, count = 4},
+        }
+    },
+    {
+        id = 28102,
+        name = "Embersage Sword",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 15},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 50},
+        }
+    },
+    {
+        id = 28103,
+        name = "Embersage Axe",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 15},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 50},
+        }
+    },
+    {
+        id = 28104,
+        name = "Embersage Club",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 15},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 50},
+        }
+    },
+    {
+        id = 28105,
+        name = "Embersage Shield",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 8},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 30},
+        }
+    },
+    {
+        id = 28110,
+        name = "Vigils Wand",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 15},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 50},
+        }
+    },
+    {
+        id = 28111,
+        name = "Vigils Spellbook",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 8},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 30},
+        }
+    },
+    {
+        id = 28116,
+        name = "Shadewalker Bow",
+        level = 25,
+        cost = 50000,
+        count = 1,
+        materials = {
+            {id = 28120, count = 20},
+            {id = 28121, count = 15},
+            {id = 28118, count = 15},
+            {id = 28123, count = 15},
+            {id = 26780, count = 50},
+        }
+    }
+}
